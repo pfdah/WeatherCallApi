cc9b1d64ffff0666654d3c484ee503ec2268b831
pratik <Pratik>
2022-06-07 21:58:52 +0545

    Made basic functioning

--- apiCall.py
+++ apiCall.py
@@ -12,4 +12,19 @@ print("Your Current IP location is: Longitude",longitude," and Latitude: ", lati
 res = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (latitude, longitude, key))
 
 data = json.loads(res.text)
-print(data['current'])
+current_weather = data['current']
+
+
+temp = current_weather['temp']
+humd = current_weather['humidity']
+
+weat = current_weather['weather'][0]
+weat_main =  weat['main']
+weat_desc =  weat['description']
+
+city = my_location.geojson['features'][0]['properties']['city']
+country = my_location.geojson['features'][0]['properties']['country']
+
+print("For ", city,country,"the temperature is ",temp,"C")
+print("The weather is ", weat_main," with ",weat_desc)
+print("The humidity is ",humd,"%")
